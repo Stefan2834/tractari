44c092c958abe18a5111d8d70949c2c909bebbd0
mares-gabriel <maresgabriel.dev@gmail.com>
2024-09-02 21:00:15 +0300

    test

--- src/components/homepage/Scroll.tsx
+++ src/components/homepage/Scroll.tsx
@@ -1,72 +1,64 @@
-import React, {useRef} from "react";
-import truck from "@/assets/photo/truck.png";
-import {useDefault} from "@/contexts/useDefault";
-import {useGSAP} from "@gsap/react";
+import React, { useRef } from "react";
+import truck from "@/assets/truck.png";
+import { useDefault } from "@/contexts/useDefault";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
-
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Scroll() {
+   const { screen } = useDefault();
+   const containerRef = useRef<any>();
 
-    const {screen} = useDefault()
-    const containerRef = useRef<any>();
+   useGSAP(
+      () => {
+         gsap.to(".moving-img", {
+            scrollTrigger: {
+               trigger: ".moving-img",
+               scrub: 3,
+               start: "top 99%",
+               end: "bottom 30%",
+            },
+            x: "100vw",
+            duration: 3,
+         });
+         gsap.to(".moving-text", {
+            scrollTrigger: {
+               trigger: ".moving-text",
+               scrub: 3,
+               start: "top 80%",
+               end: "bottom 80%",
+            },
+            x: "50vw",
+            duration: 3,
+         });
+      },
+      { scope: containerRef }
+   );
 
-    useGSAP(
-        () => {
-            gsap.to(".moving-img", {
-                scrollTrigger: {
-                    trigger: ".moving-img",
-                    scrub: 3,
-                    start: "top 99%",
-                    end: "bottom 30%",
-                },
-                x: "100vw",
-                duration: 3,
-            });
-            gsap.to(".moving-text", {
-                scrollTrigger: {
-                    trigger: ".moving-text",
-                    scrub: 3,
-                    start: "top 80%",
-                    end: "bottom 80%",
-                },
-                x: "50vw",
-                duration: 3,
-            });
-        },
-        {scope: containerRef}
-    );
-
-    return (
-        <div
-            style={{height: "800px", width: "1700px"}}
-            className="mt-48 relative flex items-center justify-start"
-            ref={containerRef}
-        >
-            <div className="moving-element">
-                <img
-                    src={truck.src}
-                    alt="truck"
-                    className="moving-img"
-                />
-                <div className="moving-text">
-                    <p className="text-5xl m:text-4xl text-black">PESTE</p>
-                    <span
-                        className="primary-color font-bold"
-                        style={{
-                            fontSize: `${screen > 600 ? "256px" : "196px"}`,
-                            lineHeight: "1.1",
-                        }}
-                    >
-							300
-						</span>
-                    <p className="text-5xl m:text-4xl text-black">
-                        clienți mulțumiți
-                    </p>
-                </div>
+   return (
+      <div
+         style={{ height: "800px", width: "1700px" }}
+         className="mt-48 relative flex items-center justify-start"
+         ref={containerRef}
+      >
+         <div className="moving-element">
+            <img src={truck.src} alt="truck" className="moving-img" />
+            <div className="moving-text">
+               <p className="text-5xl m:text-4xl text-black">PESTE</p>
+               <span
+                  className="primary-color font-bold"
+                  style={{
+                     fontSize: `${screen > 600 ? "256px" : "196px"}`,
+                     lineHeight: "1.1",
+                  }}
+               >
+                  300
+               </span>
+               <p className="text-5xl m:text-4xl text-black">clienți mulțumiți</p>
             </div>
-        </div>
-    )
+         </div>
+      </div>
+   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,20 @@ import Main from "@/components/homepage/Main";
 import Scroll from "@/components/homepage/Scroll";
 import Review from "@/components/homepage/Review";
 import Slider from "@/components/homepage/Slider";
+import Contact from "./contact";
 
 export default function Home() {
    return (
       <div className="w-full font overflow-hidden bg-white text-black">
-         {/* <Navbar /> */}
-         {/* <Main /> */}
-         {/* <Scroll /> */}
-         {/* <Review /> */}
+         <Navbar />
+         <Main />
+         <Scroll />
+         <Review />
          <h1 className="w-full text-center text-6xl mt-40 font-semibold m:text-3xl">
             Nu suntem unica firmă de tractaj
             <br /> 24/7 din România, însă...
          </h1>
-         {/* <p className="w-full text-center font-light text-3xl mt-10 m:text-xl">
+         <p className="w-full text-center font-light text-3xl mt-10 m:text-xl">
             Ne pasă de clienții noștri, și apreciem o muncă de calitate. Tractăm orice tip de
             autoturisme,
             <br />
@@ -29,9 +30,9 @@ export default function Home() {
             <br />
             oriunde în țară și la orice oră. Nu ezita să ne contactezi dacă întâmpini probleme.
             <br />
-         </p> */}
-         {/* <Slider /> */}
-         {/* <Contact /> */}
+         </p>
+         <Slider />
+         <Contact />
          <Footer />
       </div>
    );

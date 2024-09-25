456bde0e4b4850d5df0143375479bce1d1d2c3d5
Stefan2834 <iosifs617@gmail.com>
2024-09-25 18:41:06 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
     i18n: {
         locales: ["ro"],
         defaultLocale: "ro",
+    },
+    async rewrites() {
+        return [
+            {
+                source: 'http://kotasistenta.ro',
+                destination: 'https://kotasistenta.ro',
+                permanent: "true",
+            }
+        ]
     }
 };
 

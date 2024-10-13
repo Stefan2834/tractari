8b03f75847294e823e0e8fafbbb779a807efaab0
Stefan2834 <iosifs617@gmail.com>
2024-10-13 11:28:53 +0300

    seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,15 +41,6 @@ export default function App({ Component, pageProps: pageProps }: AppProps) {
                />
 
                {/* Icons */}
-               <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-               <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-               <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-               <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-               <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-               <link rel="icon" sizes="192x192" href="/android-icon-192x192.png" />
-               <link rel="icon" sizes="32x32" href="/favicon-32x32.png" />
-               <link rel="icon" sizes="96x96" href="/favicon-96x96.png" />
-               <link rel="icon" sizes="16x16" href="/favicon-16x16.png" />
                <link rel="icon" href="/favicon.ico" />
 
                {/* Verification tag */}
